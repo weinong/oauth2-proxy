2ffae73b3a4cedff8815fc2217e4c5ab2bfc3873
Joel Speed <joel.speed@hotmail.co.uk>
2020-11-07 16:30:00 +0000

    Generate reference page in configuration

---
+++ pkg/apis/options/doc.go
@@ -0,0 +1,2 @@
+//go:generate go run ../../../cmd/gen-reference --package . --types Upstreams,Header --header-file ../../../docs/docs/configuration/reference.md.tmpl --out-file ../../../docs/docs/configuration/reference.md
+package options
